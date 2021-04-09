1e7f859de91bfa8af29267ef9cbd9c3390b50a3d
JoonPark <zxqw1104@gmail.com>
2021-04-08 23:52:31 -0700

    Updated db method
    Implement Promise in PrintSeasonInfo()

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-
 const port = 4000;
+
+const db = require('../database/db');
 
 const cors = require('cors');
 
@@ -13,7 +14,12 @@ app.use(express.urlencoded({extended: true}));
 // app.use(express.static('public'));
 app.use(express.static(path.join(__dirname, 'public')));
 
+
 app.get('/', (req, res) => { res.send('hello')});
 
-app.listen(port, () => {console.log( `Path: ${(path.join(__dirname, 'public'))}\n` + `proxy listening on http://localhost:${port}`)});
+
+app.listen(port, () => {
+  console.log( `Path: ${(path.join(__dirname, 'public'))}\n` + `proxy listening on http://localhost:${port}\n` + `${db.printSeasonInfo()}`)
+
+});
 
